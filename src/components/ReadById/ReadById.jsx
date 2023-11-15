3a698ad944f352b64874525c23e0f43c3573a612
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-11-15 01:30:13 +0000

    commit readyByid

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom"
+
 export default function ReadById() {
-    return <div>ReadById
-    </div>
+    const { id } = useParams()
+
+    const apiUrl = `https:/rickandmorthyapi.com/api/character/${id}`
+
+    return <div>ReadById: {id}</div>
 }
